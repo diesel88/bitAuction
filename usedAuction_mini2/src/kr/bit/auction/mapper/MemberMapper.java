0a0e4a004bcd4e35b56ed8ee2b4d7b3e5479a008
Bit503-15 <Bit503-15@Bit503-15-PC>
2017-06-09 17:56:08 +0900

    membermapper

--- usedAuction_mini2/src/kr/bit/auction/mapper/MemberMapper.java
+++ usedAuction_mini2/src/kr/bit/auction/mapper/MemberMapper.java
@@ -3,6 +3,16 @@ package kr.bit.auction.mapper;
 import kr.bit.auction.member.vo.MemberGenericVO;
 
 public interface MemberMapper {
-
 	public void insertMember(MemberGenericVO GenericVO) throws Exception;
+	public int duplicateID(String id);
+	
+	public int newMember(MemberGenericVO vo);
+	
+	public int newComMember(MemberGenericVO vo);
+	
+	public int newPriMember(MemberGenericVO vo);
+	
+	public int newMgrMember(MemberGenericVO vo);
+	
+	public void memberInfo();
 }
